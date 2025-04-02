368acbb87d16daf5c12887bab23eb5dfe19fabd7
Corbano <ecorbano2012@gmail.com>
2025-04-02 01:08:44 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,85 +1,81 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! aprendendo POO em Java");
-    
-        
-        //trabalhando com a classe usuário
+// Classe Usuario
+class Usuario {
+    // Atributos da classe Usuario
+    int idusuario;
+    String nome;
+    String cpf;
+    int cargo;
+    String usuario;
+    String senha;
 
+    // Método para exibir os dados do usuário
+    void exibeUsuario() {
+        System.out.println("ID: " + idusuario);
+        System.out.println("Nome: " + nome);
+        System.out.println("CPF: " + cpf);
+        System.out.println("Cargo: " + cargo);
+        System.out.println("Usuário: " + usuario);
+        System.out.println("Senha: " + senha);
+        System.out.println("-------------------------");
+    }
+}
+
+// Classe DescCargo
+class DescCargo {
+    // Atributos privados da classe DescCargo
+    private int idDesc;
+    private String descricao;
+
+    // Métodos para definir os valores dos atributos
+    public void setIdDesc(int id) {
+        this.idDesc = id;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // Método para exibir os dados do cargo
+    void novoRegistro() {
+        System.out.println("Novo cargo cadastrado: " + descricao + " (ID: " + idDesc + ")");
+    }
+}
+
+// Classe principal App
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Aprendendo POO em Java");
+
+        // Criando usuário 1
         Usuario usuario1 = new Usuario();
         usuario1.idusuario = 1;
-        usuario1.nome = "jose";
+        usuario1.nome = "José";
         usuario1.cpf = "111.222.333-44";
         usuario1.cargo = 2;
         usuario1.usuario = "testerusername";
         usuario1.senha = "1234";
-               
-        //usando o metodo dentro da class usuário
-        usuario1.exibeusuario();
 
+        // Exibindo dados do usuário 1
+        usuario1.exibeUsuario();
+
+        // Criando usuário 2
         Usuario usuario2 = new Usuario();
         usuario2.idusuario = 2;
-        usuario2.nome = "maria";
+        usuario2.nome = "Maria";
         usuario2.cpf = "333.444.555-77";
         usuario2.cargo = 2;
         usuario2.usuario = "usuariomaria";
         usuario2.senha = "4561";
-               
-        //usando o metodo dentro da class usuário
-        usuario2.exibeusuario();
 
-        //-fim do uso e  instanciamento da classe usuário --
-        //----------------------------------------------------------------------------------------
+        // Exibindo dados do usuário 2
+        usuario2.exibeUsuario();
 
-
-
-
-
-
-
-        //trabalhando com a classe desccargo
-
+        // Criando e configurando um cargo
         DescCargo cargo1 = new DescCargo();
-        cargo1.setidDesc = 1;
-        cargo1.setdescricao = "Oficial";
-                       
-        //usando o metodo dentro da class usuário
+        cargo1.setIdDesc(1); // Corrigido para método correto
+        cargo1.setDescricao("Oficial"); // Corrigido para método correto
+
+        // Exibindo os dados do cargo
         cargo1.novoRegistro();
-
-        Usuario usuario2 = new Usuario();
-        usuario2.idusuario = 2;
-        usuario2.nome = "maria";
-        usuario2.cpf = "333.444.555-77";
-        usuario2.cargo = 2;
-        usuario2.usuario = "usuariomaria";
-        usuario2.senha = "4561";
-               
-        //usando o metodo dentro da class usuário
-        usuario2.exibeusuario();
-
-        //-fim do uso e  instanciamento da classe usuário --
-
-
-
-
-
-
-
-
-
-
-        // trabalhando com os metodos e atributos da classe cargo descrição
-        
-        // Criando ojbeto tipo cargo
-        // DescCargo cargo1 = new DescCargo(1, "auxiliar");
-        // cargo1.cargocriar();
-
-        // Modificando o cargo
-        // cargo1.cargomodificar(1, "escrevente");
-
-        // Exibindo as informações do cargo
-        //cargo.cargoexibir();
-
-        // Apagando o cargo
-        // cargo.cargoapagar();
     }
 }
